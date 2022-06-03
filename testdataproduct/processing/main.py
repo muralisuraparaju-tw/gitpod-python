1d260afb4c1a3c80fe896e7b431f31260922ae3c
muralisuraparaju-tw <murali.suraparaju@thoughtworks.com>
2022-06-03 12:56:10 +0000

    refactor

--- testdataproduct/processing/main.py
+++ testdataproduct/processing/main.py
@@ -1,6 +1,8 @@
+#from lib.sparkutil import get_session
 from pyspark.sql import SparkSession
 
 def test_spark():
+    #spark = get_session()
     spark = SparkSession.builder.appName("Test Spark App").getOrCreate()
     df = spark.createDataFrame([(1, "value1"), (2, "value2")], ["id", "value"])
     df.write.csv("data/output.csv")
